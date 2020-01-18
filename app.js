4c2b58f859017c2f2a8b9b71c43b045706651e59
wassimkha <khadimwassim@gmail.com>
2020-01-18 10:56:23 -0500

    f

---
+++ app.js
@@ -0,0 +1,25 @@
+//import
+const express = require('express');
+const bodyParser = require('body-parser'); // body parser
+const path = require('path');
+
+
+const app = express();
+
+//set uo templating engine
+app.set('view engine','ejs'); // to let express know what templating engine we're using
+app.set('views', 'views'); //where to find the templates
+
+
+//set up body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set up body parser
+app.use(express.static(path.join(__dirname,'public'))); //allows the public folder to be accesible by other files (read)
+
+app.use('/', (res,req,next) => {
+    
+})
+
+
+app.listen(3000);
