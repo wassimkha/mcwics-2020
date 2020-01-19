1f244fab114d87201d0ac51c1128a425ede55ff7
wassimkha <khadimwassim@gmail.com>
2020-01-18 21:14:30 -0500

    f

--- Models/friends.js
+++ Models/friends.js
@@ -1,7 +1,7 @@
 module.exports = class Profile{
 
 
-    constructor(name,lastActivity,url) {
+    constructor(name,lastActivity,url,rank) {
         this.name = name;
         this.lastActivity = lastActivity;
         this.url = url;
