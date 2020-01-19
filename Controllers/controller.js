58eb70f255f078a5e35595b383f5ccfc12f95a8d
wassimkha <khadimwassim@gmail.com>
2020-01-18 20:51:33 -0500

    f

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -19,10 +19,7 @@ exports.index = (req,res,next) => {
             
         }
     })
-    console.log(friendsArray)
-    
-
-    
+ 
     
     res.render("index", {name: currentUser, users: friendsArray});
 }
@@ -103,23 +100,10 @@ exports.postHome = (req,res,next) => {
         
         
     }
-    const usersAll = Profile.fetchAll()
-    let currentUs = null;
-    const friendsArray = [];
-    const randomAct = ['Ate a burger', 'Went Swimming', "Went to a cafe","Went hinking"];
     
-    usersAll.forEach(user => {
-        if (user.name != currentUser) {
-            var randomnumber = Math.floor(Math.random() * (3+ 1));
-            const currentFriend = new Friend(user.name,randomAct[randomnumber]);
-            friendsArray.push(currentFriend);
-            
-        }
-    })
-    res.render("index", {name: currentUser, users: friendsArray});
    
 
-    // res.redirect('/');
+    res.redirect('/');
 
 }
 
